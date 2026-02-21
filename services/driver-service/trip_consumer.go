56b3172405bae728924acb68bc694296512886ce
Rengoku1926 <prateekmahapatra1926@gmail.com>
2026-02-21 18:11:45 +0530

    added json marshaling

--- services/driver-service/trip_consumer.go
+++ services/driver-service/trip_consumer.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"context"
-	"log"
-	"ride-sharing/shared/messaging"
-
+	"encoding/json"
 	"github.com/rabbitmq/amqp091-go"
+	"log"
+	"ride-sharing/shared/contracts"
+	"ride-sharing/shared/messaging"
 )
 
 type tripConsumer struct {
@@ -19,10 +20,20 @@ func NewTripConsumer(rabbitmq *messaging.RabbitMQ) *tripConsumer {
 }
 
 func (c *tripConsumer) Listen() error {
-	return c.rabbitmq.ConsumeMessages(messaging.FindAvailableDriversQueue,
-		func(ctx context.Context, msg amqp091.Delivery) error {
-			// time.Sleep(time.Second * 15)
-			log.Printf("driver received message: %v", msg)
-			return nil
-		})
+	return c.rabbitmq.ConsumeMessages(messaging.FindAvailableDriversQueue, func(ctx context.Context, msg amqp091.Delivery) error {
+		var tripEvent contracts.AmqpMessage
+		if err := json.Unmarshal(msg.Body, &tripEvent); err != nil {
+			log.Printf("Failed to unmarshal message: %v", err)
+			return err
+		}
+
+		var payload messaging.TripEventData
+		if err := json.Unmarshal(tripEvent.Data, &payload); err != nil {
+			log.Printf("Failed to unmarshal message: %v", err)
+			return err
+		}
+
+		log.Printf("driver received message: %+v", payload)
+		return nil
+	})
 }
